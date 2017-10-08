4fb303faae129a739a0ece13e4013b9b14bdfd86
Ty Vaughan <tyvaughan@Ty-Vaughans-MacBook-Pro.local>
2017-10-08 13:49:58 -0400

    Updated files

--- DataCleanser.py
+++ DataCleanser.py
@@ -14,19 +14,39 @@ class DataCleanser:
         ParamInfo = {}
         ICUs = []
 
+        icu_index = (0,0)
+        params_index = (0,0)
+
         f = open('Specifications.txt')
         lines = f.readlines()
+        
+        # Determine the indices for gathering information.
+        for i in range(len(lines)):
+            line = lines[i].strip()
+            if(line == '#ICUs'):
+                i += 2
+                icu_index = (i,0)
+                while(lines[i].strip() != ''):
+                    i += 1
+                icu_index = (icu_index[0],i)
+            
+            if(line == '#Parameters'):
+                i += 2
+                params_index = (i,0)
+                while(lines[i].strip() != ''):
+                    i += 1
+                params_index = (params_index[0],i)
 
         # Obtain ICU Information
         regex = '\s*(\w+)\s*(\w+)\s*'
-        for line in lines[2:8]:
+        for line in lines[icu_index[0]:icu_index[1]]:
             info = re.match(regex,line)
             if(info.group(2) == 'True'):
                 ICUs.append(info.group(1))
 
         # Obtain Parameter Information
         regex = '\s*(\w+);\s*([\w-]+);\s*(\w+);\s*(\[[\d,\s]*\])\s*'
-        for line in lines[12:]:
+        for line in lines[params_index[0]:params_index[1]]:
             info = re.match(regex,line)
 
             # Obtain parameter IDs.
@@ -42,7 +62,7 @@ class DataCleanser:
                 'unit': info.group(3),
                 'ids': ids
             }
-        
+
         return ICUs, ParamInfo
 
 
