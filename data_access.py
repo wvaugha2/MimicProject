1a97d0fac48ada27b4cc05a7e490c60e3779f74f
Ty Vaughan <tyvaughan@tysnewmacbook.nomads.utk.edu>
2018-02-15 13:04:36 -0500

    Modified files.

--- data_access.py
+++ data_access.py
@@ -58,7 +58,7 @@ def obtainData(ICUInfo, ParamInfo, PatientInfo, cur, ptp):
     ptp.executeFunc(
         func=obtainMeasurements, 
         args=[m_ids, measurementquery], 
-        splitargs=[patients[:10]])
+        splitargs=[patients])
     patientlist = ptp.getResults()
     print('Obtained measurements from database: {:10.2f} seconds'.format(time.time() - atime))
 
